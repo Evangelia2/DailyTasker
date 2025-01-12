d3ac80640bbce485a0c526c9449f445407daeecc
Evangelia Baliou <134657064+Evangelia2@users.noreply.github.com>
2025-01-12 05:34:25 +0200

    Add files via upload

--- src/main/java/gr/hua/dit/android/dailytasker/TaskAdapter.java
+++ src/main/java/gr/hua/dit/android/dailytasker/TaskAdapter.java
@@ -4,11 +4,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import androidx.cardview.widget.CardView;
+
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
+import java.util.Random;
 
 public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder> {
 
@@ -29,9 +33,32 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder
     @Override
     public void onBindViewHolder(@NonNull TaskViewHolder holder, int position) {
         Task task = tasks.get(position);
+
+        // Set task details
         holder.shortNameTextView.setText(task.getShortName());
         holder.taskIdTextView.setText("ID: " + task.getUid());
         holder.statusTextView.setText(task.getStatus());
+
+        // Define a list of colors
+        int[] colors = {
+                R.color.task_red,
+                R.color.task_blue,
+                R.color.task_yellow,
+                R.color.task_green,
+                R.color.task_purple,
+                R.color.task_orange,
+                R.color.task_cyan
+        };
+
+        // Generate a random color
+        int randomColor = colors[new Random().nextInt(colors.length)];
+
+        // Set the random color as the background of the CardView
+        holder.taskCard.setCardBackgroundColor(
+                ContextCompat.getColor(holder.itemView.getContext(), randomColor)
+        );
+
+
     }
 
     @Override
@@ -49,12 +76,15 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder
         TextView shortNameTextView;
         TextView taskIdTextView;
         TextView statusTextView;
+        CardView taskCard;
 
         public TaskViewHolder(@NonNull View itemView) {
             super(itemView);
             shortNameTextView = itemView.findViewById(R.id.textViewShortName);
             taskIdTextView = itemView.findViewById(R.id.textViewTaskId);
             statusTextView = itemView.findViewById(R.id.textViewStatus);
+            taskCard = itemView.findViewById(R.id.taskCard);
+
         }
     }
 
